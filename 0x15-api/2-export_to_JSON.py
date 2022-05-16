1d5b2fe8a8ea37d83d8b4449a47494819c5ec80c
Carlos Bentin <cbentin12@gmail.com>
2022-05-16 17:58:24 -0500

    chancing to requets module

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,31 +6,28 @@
 
 
 if __name__ == '__main__':
-    import csv
     import json
+    import requests as rq
     from sys import argv as arguments
-    from urllib import request as rq
 
     employeeID = arguments[1]
     user_bio = {}
     todo_list = []
     status_dict = {}
     status_dict[employeeID] = []
-    _url_1 = 'https://jsonplaceholder.typicode.com/users'
-    _url_2 = 'https://jsonplaceholder.typicode.com/todos'
+    url_1 = 'https://jsonplaceholder.typicode.com/users'
+    url_2 = 'https://jsonplaceholder.typicode.com/todos'
 
-    with rq.urlopen('{}/{}'.format(_url_1, employeeID)) as urlObj:
-        user_bio = json.loads(urlObj.read())
+    user_bio = rq.get('{}/{}'.format(url_1, employeeID)).json()
+    todo_response = rq.get(url_2).json()
 
-    with rq.urlopen(_url_2) as urlObj:
-        response = json.loads(urlObj.read())
-        for r in response:
-            if r['userId'] == int(employeeID):
-                todo_list.append(r)
+    for r in todo_response:
+        if r['userId'] == int(employeeID):
+            todo_list.append(r)
 
     for task in todo_list:
         row = {"task": task['title'], "completed": task['completed'],
-               "username": user_bio['name']}
+               "username": user_bio['username']}
         status_dict[employeeID].append(row)
 
     with open('{}.json'.format(employeeID), 'w') as json_file:
