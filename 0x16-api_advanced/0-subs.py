598bf9664fd2cd12e2f96c8b5d3a993b19950f1c
Carlos Bentin <cbentin12@gmail.com>
2022-05-31 14:20:59 -0500

    fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,15 @@
 
 import requests
 
-userAgent = 'Python.wsl2.windows.ApiProject:v1 (by Dry-Improvement-3814)'
+
 
 
 def number_of_subscribers(subreddit):
     """
         Returns number of subscribers for given subredit
     """
+    userAgent = 'Python.wsl2.windows.ApiProject:v1 (by Dry-Improvement-3814)'
+
     if subreddit is None or type(subreddit) is not str:
         return 0
 
